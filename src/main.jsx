f44364841a8670242faba71062aa16a69eef2da3
AbdalrhmanAmmar <bodyty2n@gmail.com>
2023-07-16 00:07:10 +0200

    barchar

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from "./App";
+import BarChart from "./pages/Barchart/Barchart";
 // import Dashboard from "./page/dashboard/Dashboard";
 // import Team from "./page/team/Team";
 // import Contacts from "./page/contacts/Contacts";
@@ -16,7 +17,6 @@ import App from "./App";
 // import Form from "./page/form/Form";
 // import Calendar from "./page/calendar/Calendar";
 // import FAQ from "./page/faq/FAQ";
-// import BarChart from "./page/barChart/BarChart";
 // import PieChart from "./page/pieChart/PieChart";
 // import LineChart from "./page/lineChart/LineChart";
 // import Geography from "./page/geography/Geography";
@@ -25,16 +25,16 @@ import App from "./App";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      {/* <Route index element={<Dashboard />} /> */}
-      {/* <Route path="team" element={<Team />} />
+      {/* <Route index element={<Dashboard />} />
+      <Route path="team" element={<Team />} />
 
       <Route path="contacts" element={<Contacts />} />
       <Route path="invoices" element={<Invoices />} />
       <Route path="form" element={<Form />} />
       <Route path="calendar" element={<Calendar />} />
-      <Route path="faq" element={<FAQ />} />
-      <Route path="bar" element={<BarChart />} />
-      <Route path="pie" element={<PieChart />} />
+      <Route path="faq" element={<FAQ />} /> */}
+      <Route path="/bar" element={<BarChart />} />
+      {/* <Route path="pie" element={<PieChart />} />
       <Route path="line" element={<LineChart />} />
       <Route path="geography" element={<Geography />} />
 
