0ef241a0fb7c53f7f8a177537b02638150870059
AbdalrhmanAmmar <bodyty2n@gmail.com>
2023-07-17 02:17:45 +0200

    form

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -1,13 +1,23 @@
-import { Box, Button, MenuItem, Stack, TextField } from "@mui/material";
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Alert, Button, MenuItem, Snackbar, Stack } from "@mui/material";
+import { useForm } from "react-hook-form";
 
-const currencies = [
+const regEmail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const data = [
   {
     value: "Admin",
     label: "Admin",
   },
   {
-    value: "Manager",
-    label: "Manager",
+    value: "Manger",
+    label: "Manger",
   },
   {
     value: "User",
@@ -15,61 +25,142 @@ const currencies = [
   },
 ];
 
-function Form() {
-  return (
-    <Box
-      component="form"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 3,
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Stack direction={"row"} sx={{ gap: 2 }}>
-        <TextField
-          sx={{ flex: 1 }}
-          id="filled-basic"
-          label="First name"
-          variant="filled"
-        />
-        <TextField
-          sx={{ flex: 1 }}
-          id="filled-basic"
-          label="Last Name"
-          variant="filled"
-        />
-      </Stack>
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-      <TextField id="filled-basic" label="Email" variant="filled" />
-      <TextField id="filled-basic" label="Contact Number" variant="filled" />
-      <TextField id="filled-basic" label="Address1" variant="filled" />
-      <TextField id="filled-basic" label="Address2" variant="filled" />
-      <TextField
-        variant="filled"
-        id="outlined-select-currency"
-        select
-        label="Select"
-        defaultValue="User"
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const onSubmit = () => {
+    console.log("doneeeeeeeeeeee");
+
+    handleClick();
+  };
+
+  return (
+    <Box>
+
+      <Box
+        onSubmit={handleSubmit(onSubmit)}
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+        noValidate
+        autoComplete="off"
       >
-        {currencies.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-      </TextField>
-      <Box sx={{ textAlign: "right" }}>
-        <Button
-          type="submit"
-          sx={{ textTransform: "capitalize" }}
-          variant="contained"
+        <Stack sx={{ gap: 2 }} direction={"row"}>
+          <TextField
+            error={Boolean(errors.firstName)}
+            helperText={
+              Boolean(errors.firstName)
+                ? "This field is required & min 3 character"
+                : null
+            }
+            {...register("firstName", { required: true, minLength: 3 })}
+            sx={{ flex: 1 }}
+            label="First Name"
+            variant="filled"
+          />
+
+          <TextField
+            error={Boolean(errors.lastName)}
+            helperText={
+              Boolean(errors.lastName)
+                ? "This field is required & min 3 character"
+                : null
+            }
+            {...register("lastName", { required: true, minLength: 3 })}
+            sx={{ flex: 1 }}
+            label="Last Name"
+            variant="filled"
+          />
+        </Stack>
+
+        <TextField
+          error={Boolean(errors.email)}
+          helperText={
+            Boolean(errors.email)
+              ? "Please provide a valid email address"
+              : null
+          }
+          {...register("email", { required: true, pattern: regEmail })}
+          label="Email"
+          variant="filled"
+        />
+
+        <TextField
+          error={Boolean(errors.ContactNumber)}
+          helperText={
+            Boolean(errors.ContactNumber)
+              ? "Please provide a valid Phone number"
+              : null
+          }
+          {...register("ContactNumber", {
+            required: true,
+            pattern: phoneRegExp,
+          })}
+          label="Contact Number"
+          variant="filled"
+        />
+        <TextField label="Adress 1" variant="filled" />
+        <TextField label="Adress 2" variant="filled" />
+
+        <TextField
+          variant="filled"
+          id="outlined-select-currency"
+          select
+          label="Role"
+          defaultValue="User"
         >
-          Create New User
-        </Button>
+          {data.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <Box sx={{ textAlign: "right" }}>
+          <Button
+            type="submit"
+            sx={{ textTransform: "capitalize" }}
+            variant="contained"
+          >
+            Create New User
+          </Button>
+
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}
+          >
+            <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
+              Account created successfully
+            </Alert>
+          </Snackbar>
+        </Box>
       </Box>
     </Box>
   );
-}
+};
 
 export default Form;
