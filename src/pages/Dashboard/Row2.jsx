d76e138b9401ea05767c3d3c2c49212b9f754a85
AbdalrhmanAmmar <bodyty2n@gmail.com>
2023-07-22 03:38:28 +0200

    dash

--- src/pages/Dashboard/Row2.jsx
+++ src/pages/Dashboard/Row2.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Box, Paper, Stack, Typography, useTheme } from "@mui/material";
+import Line from "./../LineChart/Line";
 
 function Row2() {
+  const theme = useTheme();
   return (
-    <div>Row2</div>
-  )
+    <Stack direction={"row"}>
+      <Paper sx={{ width: "990px" }}>
+        <Line />
+      </Paper>
+      <Box>
+        <Typography
+          color={theme.palette.secondary.main}
+          fontWeight={"bold"}
+          p={1.2}
+          variant="h6"
+        >
+          React Transactions
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
 
-export default Row2
+export default Row2;
